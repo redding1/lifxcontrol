bbf4ddc2aa11794d2fcd3346d120ae92dca9b893
redding1 <matthewreddin@gmail.com>
2015-11-03 22:31:41 +1100

    c

--- redhome.py
+++ redhome.py
@@ -80,7 +80,8 @@ def main():
         #print addressInNetwork(address,networkb)        
         nm= nmap.PortScanner()
         try:
-            nm.scan(Matts_iPhone_IP,'80', '-n -sS -T5')
+            #nm.scan(Matts_iPhone_IP,'80', '-n -sS -T5')
+            nm.scan(Matts_iPhone_IP,'80')
             #nm[Matts_Iphone_IP].state()
             print "found IP"
             connect_host = 1
