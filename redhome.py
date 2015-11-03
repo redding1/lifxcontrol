df600f59f2edf4a27bf5dbccf5941f5e0bf9c88d
redding1 <matthewreddin@gmail.com>
2015-11-04 10:19:45 +1100

    v0.1

--- redhome.py
+++ redhome.py
@@ -100,24 +100,7 @@ def main():
 #if "Living" in devices[i].get_label()       
 #living.append(devices[i])
 
-
-
-def makeMask(n):
-    "return a mask of n bits as a long integer"
-    return (2L<<n-1) - 1
-
-def dottedQuadToNum(ip):
-    "convert decimal dotted quad string to long integer"
-    return struct.unpack('L',socket.inet_aton(ip))[0]
-
-def networkMask(ip,bits):
-    "Convert a network address to a long integer" 
-    return dottedQuadToNum(ip) & makeMask(bits)
-
-def addressInNetwork(ip,net):
-   "Is an address in a network"
-   return ip & net == net
-       
+      
 def toggle_device_power(device, interval=0.5, num_cycles=3): #TEST
     original_power_state = device.get_power()
     device.set_power("off")
